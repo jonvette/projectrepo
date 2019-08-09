8654fa7e0becd49f6034ff809c5dbce1ce142ceb
kurtispe <kurtispedersen42@gmail.com>
2019-08-08 22:45:25 -0700

    add cors

--- Apps/WebApi/Startup.cs
+++ Apps/WebApi/Startup.cs
@@ -27,6 +27,8 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(x => x.AddDefaultPolicy(builder => { builder.AllowAnyOrigin(); }));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<ListContext>(options => options.UseSqlServer(Configuration.GetConnectionString("XyzTechonologyDb")));
         }
